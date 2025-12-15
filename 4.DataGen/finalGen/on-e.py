92b13cab93f851931399dfb5c1d74a320866ecdb
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-12-15 06:24:44 +0100

    Update on-e.py

--- 4.DataGen/finalGen/on-e.py
+++ 4.DataGen/finalGen/on-e.py
@@ -60,7 +60,7 @@ class Analysis():
         self.prod_tag = 'FCCee/winter2023/IDEA/'
 
         # Optional: output directory
-        self.output_dir = 'electron_yukawa/stage1'
+        #self.output_dir = 'electron_yukawa/stage1'
 
         # Optional: analysis name
         self.analysis_name = 'Electron Yukawa Analysis'
